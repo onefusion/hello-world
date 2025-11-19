12295828ed4eb5a6c1cccbd3c73da4d87f1c31ed
Sean Ringrose <seanringrose12@gmail.com>
2025-11-19 13:02:13 -0600

    Add first exercise for 01_basics

---
+++ 01_basics/intro.py
@@ -0,0 +1,8 @@
+# Ask for name
+name = input("What's your name? ")
+
+# Ask for favorite game
+favorite_game = input("What's your favorite game? ")
+
+# Print formatted message using both inputs
+print(f"Nice to meet you, {name}! {favorite_game} is a great choice.")
---
+++ 06_mini_projects/README.md
@@ -0,0 +1,4 @@
+Pick small, fun projects that connect to you:
+Loot generator: given rarity categories, randomly output loot.
+Simple CLI to-do list using a JSON or text file as storage.
+Encounter roller for your TTRPG setting.
